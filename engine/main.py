7f74cccc6a19b9eb591ba13363f6605fe752fb6a
aahmed <aahmed@lb.com>
2019-11-26 09:48:24 -0500

    added requirements.txt, changed the token to be passed from CLI

--- engine/main.py
+++ engine/main.py
@@ -3,7 +3,7 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from .threadcontrol import ThreadControl
+from engine.threadcontrol import ThreadControl
 
 
 def main(argv):
