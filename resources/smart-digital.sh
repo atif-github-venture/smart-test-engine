bb61366429560b6f40fc54bb3d2dfe412addc3d0
aahmed <aahmed@lb.com>
2019-11-29 20:36:04 -0500

    tetsing docker-compose trigger from shell - 4

--- resources/smart-digital.sh
+++ resources/smart-digital.sh
@@ -7,7 +7,7 @@ echo "$2"
 
 docker-machine --debug create \
   --driver digitalocean \
-  --digitalocean-region "nyc1" \
+  --digitalocean-region "sfo1" \
   --digitalocean-size "s-1vcpu-1gb" \
   --digitalocean-access-token $DIGITAL_OCEAN_ACCESS_TOKEN \
   $1;
