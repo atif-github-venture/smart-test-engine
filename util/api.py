c058d6819a472156e544291e5bb78b65914d7921
aahmed <aahmed@lb.com>
2020-01-16 22:59:37 -0500

    adding test dir for experiment

--- util/api.py
+++ util/api.py
@@ -15,62 +15,66 @@ class Api:
         self.temppath = save_to_file_path
 
     def getcall_tests(self):
-        config = configparser.RawConfigParser()
-        config.read(os.path.join(os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir)),
-                                 'resources', 'configfile.properties'))
-        urllib3.disable_warnings()
-        uri = 'http://' + config.get('API', 'api.base-uri') + config.get('API', self.servicename)
-        PARAMS = {'tags': self.filter.split(',')}
-        headers = {'content-type': 'application/json', 'project': self.project}
-        r = requests.get(url=uri, params=PARAMS, headers=headers, verify=False)
-        if r.status_code == 200:
-            data = r.json()
-            print(data)
-            write_json(data, self.temppath)
+        pass
+        # config = configparser.RawConfigParser()
+        # config.read(os.path.join(os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir)),
+        #                          'resources', 'configfile.properties'))
+        # urllib3.disable_warnings()
+        # uri = 'http://' + config.get('API', 'api.base-uri') + config.get('API', self.servicename)
+        # PARAMS = {'tags': self.filter.split(',')}
+        # headers = {'content-type': 'application/json', 'project': self.project}
+        # r = requests.get(url=uri, params=PARAMS, headers=headers, verify=False)
+        # if r.status_code == 200:
+        #     data = r.json()
+        #     print(data)
+        #     write_json(data, self.temppath)
 
     def getcall_run_conf(self):
-        config = configparser.RawConfigParser()
-        config.read(os.path.join(os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir)),
-                                 'resources', 'configfile.properties'))
-        urllib3.disable_warnings()
-        uri = 'http://' + config.get('API', 'api.base-uri') + config.get('API', self.servicename)
-        PARAMS = {'testconfig': self.filter}
-        headers = {"content-type": "application/json", "project": self.project}
-        r = requests.get(url=uri, params=PARAMS, headers=headers, verify=False)
-        if r.status_code == 200:
-            data = r.json()
-            print(data)
-            write_json(data, self.temppath)
-        else:
-            print(r.status_code)
+        pass
+        # config = configparser.RawConfigParser()
+        # config.read(os.path.join(os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir)),
+        #                          'resources', 'configfile.properties'))
+        # urllib3.disable_warnings()
+        # uri = 'http://' + config.get('API', 'api.base-uri') + config.get('API', self.servicename)
+        # PARAMS = {'testconfig': self.filter}
+        # headers = {"content-type": "application/json", "project": self.project}
+        # r = requests.get(url=uri, params=PARAMS, headers=headers, verify=False)
+        # if r.status_code == 200:
+        #     data = r.json()
+        #     print(data)
+        #     write_json(data, self.temppath)
+        # else:
+        #     print(r.status_code)
 
     def getcall_data_namespace(self):
-        config = configparser.RawConfigParser()
-        config.read(os.path.join(os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir)),
-                                 'resources', 'configfile.properties'))
-        urllib3.disable_warnings()
-        uri = 'http://' + config.get('API', 'api.base-uri') + config.get('API', self.servicename)
-        # PARAMS = {'name': self.filter}
-        PARAMS = {}
-        headers = {"content-type": "application/json", "project": self.project}
-        r = requests.get(url=uri, params=PARAMS, headers=headers, verify=False)
-        if r.status_code == 200:
-            data = r.json()
-            print(data)
-            write_json(data, self.temppath)
-        else:
-            print(r.status_code)
+        pass
+        # config = configparser.RawConfigParser()
+        # config.read(os.path.join(os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir)),
+        #                          'resources', 'configfile.properties'))
+        # urllib3.disable_warnings()
+        # uri = 'http://' + config.get('API', 'api.base-uri') + config.get('API', self.servicename)
+        # # PARAMS = {'name': self.filter}
+        # PARAMS = {}
+        # headers = {"content-type": "application/json", "project": self.project}
+        # r = requests.get(url=uri, params=PARAMS, headers=headers, verify=False)
+        # if r.status_code == 200:
+        #     data = r.json()
+        #     print(data)
+        #     write_json(data, self.temppath)
+        # else:
+        #     print(r.status_code)
 
     def getcall_repository(self):
-        config = configparser.RawConfigParser()
-        config.read(os.path.join(os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir)),
-                                 'resources', 'configfile.properties'))
-        urllib3.disable_warnings()
-        uri = 'http://' + config.get('API', 'api.base-uri') + config.get('API', self.servicename)
-        PARAMS = {'identifier': self.filter}
-        headers = {'content-type': 'application/json', 'project': self.project}
-        r = requests.get(url=uri, params=PARAMS, headers=headers, verify=False)
-        if r.status_code == 200:
-            return r.json()[0]['property']
-        else:
-            return None
+        pass
+        # config = configparser.RawConfigParser()
+        # config.read(os.path.join(os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir)),
+        #                          'resources', 'configfile.properties'))
+        # urllib3.disable_warnings()
+        # uri = 'http://' + config.get('API', 'api.base-uri') + config.get('API', self.servicename)
+        # PARAMS = {'identifier': self.filter}
+        # headers = {'content-type': 'application/json', 'project': self.project}
+        # r = requests.get(url=uri, params=PARAMS, headers=headers, verify=False)
+        # if r.status_code == 200:
+        #     return r.json()[0]['property']
+        # else:
+        #     return None
