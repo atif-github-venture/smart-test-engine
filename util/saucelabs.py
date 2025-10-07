b2db70d2c623ff8234ad9a99cb4a8ad40c20bbfd
Atif Ahmed <atif.project@gmail.com>
2025-10-07 09:56:39 -0400

    Update saucelabs.py

--- util/saucelabs.py
+++ util/saucelabs.py
@@ -4,5 +4,5 @@ from sauceclient import SauceClient
 class SauceLabs:
 
     def update_test_info(j, b, p):
-        sc = SauceClient('fatebamboo', '9099ed6e-b6ab-42d0-a2d1-76fe26985c74')
+        sc = SauceClient('user', 'key')
         sc.jobs.update_job(job_id=j, build=b, passed=p)
